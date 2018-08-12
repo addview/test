387fc59d43c8e8bcfafbc4af42804f8842fdd66c
Fredrik Persson <fredrik@slowmotion.se>
2018-08-12 10:22:39 +0200

    krrrrrr igen

---
+++ dsdsdsds.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class dsdsdsds extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+dsdsdsds.propTypes = {
+
+};
+
+export default dsdsdsds;
