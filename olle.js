93f34be5dc1940e21fcf372849fbdd8d226b1cc3
Fredrik Persson <fredrik@slowmotion.se>
2018-08-12 10:54:16 +0200

    lokal commit olle

---
+++ olle.js
@@ -0,0 +1,15 @@
+RTCIceCandidate
+
+import React, { Component } from 'react';
+
+class olle extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default olle;
